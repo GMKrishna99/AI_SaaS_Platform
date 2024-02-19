65365cfc5303cc334d2b4e574ba29153f2656c78
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-02-19 13:29:40 +0530

    chore : wrapped with clerk provider for auth and removed un used font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Nunito, IBM_Plex_Sans } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IBM_PLEX = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,10 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBM_PLEX antialiased", IBM_PLEX.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-IBM_PLEX antialiased", IBM_PLEX.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
