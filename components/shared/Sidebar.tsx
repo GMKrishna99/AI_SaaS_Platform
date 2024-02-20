8f4846864208dc31af1083c200e0141c9c09d94f
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-02-20 10:50:29 +0530

    chore : sliced some routs to get in bottom and added some of them in bottom

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
           <SignedIn>
             <ul className="sidebar-nav_elements">
               {/* sidebar components */}
-              {navLinks.map((link) => {
+              {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route == pathname;
                 return (
                   <li
@@ -46,7 +46,33 @@ const Sidebar = () => {
                   </li>
                 );
               })}
-              {/* logout button to logout user */}
+            </ul>
+            {/* logout button to logout user */}
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route == pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group cursor-pointer ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
               <li className="flex-center cursor-pointer gap-2 p-4">
                 <UserButton afterSignOutUrl="/" showName />
               </li>
