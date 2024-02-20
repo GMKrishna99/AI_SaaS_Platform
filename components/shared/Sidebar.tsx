534667072829f2b61f31c839a4d5dfbbb33e606e
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-02-20 10:35:46 +0530

    feat : added sidebar-nav components for signed-in used

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,15 +1,50 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import LogoSVG from "@/public/assets/images/logo-text.svg";
 import Image from "next/image";
+import { SignedIn } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
           <Image src={LogoSVG} alt="logo" width={180} height={28} />
         </Link>
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route == pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group cursor-pointer ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
       </div>
     </aside>
   );
