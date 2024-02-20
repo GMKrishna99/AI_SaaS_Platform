7e2015661372ca8a9bd9ecc86271dbc7c4acbb16
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-02-20 09:37:20 +0530

    chore : user button added to check the account is logged or not

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <>
-      <h1>Main page</h1>
+      <h1>Home Page</h1>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 }
