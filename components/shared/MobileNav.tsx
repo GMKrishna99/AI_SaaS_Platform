78543a253598f6d8c8666002f1dc21f93a37333a
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-02-20 11:01:10 +0530

    feat : added sheet form shadcn to display mobile nav

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -7,16 +7,38 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+
 import Link from "next/link";
-import LogoSmallSVG from "@/public/assets/images/logo-text.svg";
 import Image from "next/image";
 
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import HamburgerSVG from "@/public/assets/icons/menu.svg";
+import LogoSmallSVG from "@/public/assets/images/logo-text.svg";
 const MobileNav = () => {
   return (
     <header className="header">
       <Link href="/" className="flex items-center gap-2 md:py-2 ">
         <Image src={LogoSmallSVG} alt="logo" width={180} height={28} />
       </Link>
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <Image src={HamburgerSVG} alt="menu" width={24} height={24} />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+      </nav>
     </header>
   );
 };
