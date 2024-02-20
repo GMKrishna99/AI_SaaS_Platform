920376f0137d0d3d9ce8684adfc824cfe78bcc1f
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-02-20 10:24:41 +0530

    chore : added logo

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
 import React from "react";
+import LogoSVG from "@/public/assets/images/logo-text.svg";
+import Image from "next/image";
 
 const Sidebar = () => {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image src={LogoSVG} alt="logo" width={180} height={28} />
+        </Link>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
