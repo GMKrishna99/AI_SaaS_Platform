8b06fe11d15920dbfd837c3b29ecd2126a1f937d
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-02-20 10:57:04 +0530

    chore : import sheet and added logos

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Link from "next/link";
+import LogoSmallSVG from "@/public/assets/images/logo-text.svg";
+import Image from "next/image";
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2 md:py-2 ">
+        <Image src={LogoSmallSVG} alt="logo" width={180} height={28} />
+      </Link>
+    </header>
+  );
 };
 
 export default MobileNav;
