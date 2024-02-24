cb130c423d74f58b2a23051262e0ea240824d796
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-02-24 13:01:45 +0530

    refactor : spell mistake

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -11,12 +11,12 @@ interface MongooseConnection {
 }
 // Declaring a variable named 'cashed' of type 'MongooseConnection'
 // and assigning it the value of '(global as any).mongoose'
-let cashed: MongooseConnection = (global as any).mongoose;
+let cached: MongooseConnection = (global as any).mongoose;
 
 // Checking if the 'cashed' variable is falsy (null or undefined)
-if (!cashed) {
+if (!cached) {
   // If 'cashed' is falsy, initialize it and assign it to '(global as any).mongoose'
-  cashed = (global as any).mongoose = {
+  cached = (global as any).mongoose = {
     conn: null, // Initialize 'conn' property as null
     promise: null, // Initialize 'promise' property as null
   };
@@ -25,20 +25,20 @@ if (!cashed) {
 // Exporting an asynchronous function named 'connectToDatabase'
 export const connectToDatabase = async () => {
   // If 'cashed.conn' already exists, return it
-  if (cashed.conn) return cashed.conn;
+  if (cached.conn) return cached.conn;
 
   // If 'MONGODB_URL' is not provided, throw an error
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
   // If 'cashed.promise' doesn't exist, initialize it
-  cashed.promise =
-    cashed.promise ||
+  cached.promise =
+    cached.promise ||
     // Use mongoose.connect to establish a connection to MongoDB
     mongoose.connect(MONGODB_URL, { dbName: "aiapp", bufferCommands: false });
 
   // Wait for the promise in cashed.promise to resolve (i.e., for the MongoDB connection to be established)
   // and assign the resolved value (the Mongoose connection object) to the cashed.conn property
-  cashed.conn = await cashed.promise;
+  cached.conn = await cached.promise;
 
-  return cashed.conn; // Return the 'cashed.conn' value after the connection is established
+  return cached.conn; // Return the 'cashed.conn' value after the connection is established
 };
